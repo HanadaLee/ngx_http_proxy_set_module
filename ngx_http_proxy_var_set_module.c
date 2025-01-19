a7720ccd00fc207a5ad5c34c2c6d551768c2ded6
Hanada <im@hanada.info>
2025-01-20 01:59:01 +0800

    feature: support grpc proxy

--- ngx_http_proxy_var_set_module.c
+++ ngx_http_proxy_var_set_module.c
@@ -18,6 +18,7 @@ typedef struct {
 
 typedef struct {
     ngx_array_t               *vars;
+    ngx_array_t               *grpc_vars;
 } ngx_http_proxy_var_set_loc_conf_t;
 
 
@@ -38,6 +39,15 @@ static ngx_command_t  ngx_http_proxy_var_set_commands[] = {
       NGX_HTTP_LOC_CONF_OFFSET,
       0,
       NULL },
+
+    { ngx_string("grpc_var_set"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE23,
+      ngx_http_grpc_var_set,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      0,
+      NULL },
+
+    /* TODO: support other upstream content handlers */
 
       ngx_null_command
 };
@@ -85,7 +95,7 @@ ngx_http_proxy_var_set_handler(ngx_http_request_t *r)
     ngx_http_core_main_conf_t           *cmcf;
 
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "log var set handler");
+                   "proxy var set handler");
 
     plcf = ngx_http_get_module_loc_conf(r, ngx_http_proxy_var_set_module);
 
@@ -98,6 +108,82 @@ ngx_http_proxy_var_set_handler(ngx_http_request_t *r)
 
     pv = plcf->vars->elts;
     last = pv + plcf->vars->nelts;
+
+    while (pv < last) {
+
+        if (pv->filter) {
+            if (ngx_http_complex_value(r, pv->filter, &val)
+                    != NGX_OK) {
+                return NGX_ERROR;
+            }
+
+            if (val.len == 0 || (val.len == 1 && val.data[0] == '0')) {
+                if (!pv->negative) {
+                    continue;
+                }
+            } else {
+                if (pv->negative) {
+                    continue;
+                }
+            }
+        }
+
+        /*
+         * explicitly set new value to make sure it will be available after
+         * internal redirects
+         */
+
+        vv = &r->variables[pv->index];
+
+        if (ngx_http_complex_value(r, &pv->value, &val) != NGX_OK) {
+            return NGX_ERROR;
+        }
+
+        vv->valid = 1;
+        vv->not_found = 0;
+        vv->data = val.data;
+        vv->len = val.len;
+
+        if (pv->set_handler) {
+            /*
+             * set_handler only available in cmcf->variables_keys, so we store
+             * it explicitly
+             */
+
+            pv->set_handler(r, vv, v[pv->index].data);
+        }
+
+        pv++;
+    }
+
+    return NGX_OK;
+}
+
+
+ngx_int_t
+ngx_http_grpc_var_set_handler(ngx_http_request_t *r)
+{
+    ngx_str_t                            val;
+    ngx_http_variable_t                 *v;
+    ngx_http_variable_value_t           *vv;
+    ngx_http_proxy_var_set_loc_conf_t   *plcf;
+    ngx_http_proxy_var_set_variable_t   *pv, *last;
+    ngx_http_core_main_conf_t           *cmcf;
+
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                   "grpc var set handler");
+
+    plcf = ngx_http_get_module_loc_conf(r, ngx_http_proxy_var_set_module);
+
+    if (plcf->grpc_vars == NULL) {
+        return NGX_OK;
+    }
+
+    cmcf = ngx_http_get_module_main_conf(r, ngx_http_core_module);
+    v = cmcf->variables.elts;
+
+    pv = plcf->grpc_vars->elts;
+    last = pv + plcf->grpc_vars->nelts;
 
     while (pv < last) {
 
@@ -212,21 +298,126 @@ ngx_http_proxy_var_set(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         return NGX_CONF_ERROR;
     }
 
-    if (cf->args->nelts == 3) {
+    if (cf->args->nelts == 4) {
 
-        if (ngx_strncmp(value[2].data, "if=", 3) == 0) {
-            s.len = value[2].len - 3;
-            s.data = value[2].data + 3;
+        if (ngx_strncmp(value[3].data, "if=", 3) == 0) {
+            s.len = value[3].len - 3;
+            s.data = value[3].data + 3;
             pv->negative = 0;
 
-        } else if (ngx_strncmp(value[2].data, "if!=", 4) == 0) {
-            s.len = value[2].len - 4;
-            s.data = value[2].data + 4;
+        } else if (ngx_strncmp(value[3].data, "if!=", 4) == 0) {
+            s.len = value[3].len - 4;
+            s.data = value[3].data + 4;
             pv->negative = 1;
 
         } else {
             ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
-                "invalid parameter \"%V\"", &value[2]);
+                "invalid parameter \"%V\"", &value[3]);
+            return NGX_CONF_ERROR;
+        }
+
+        ngx_memzero(&ccv, sizeof(ngx_http_compile_complex_value_t));
+
+        ccv.cf = cf;
+        ccv.value = &s;
+        ccv.complex_value = ngx_palloc(cf->pool,
+                                    sizeof(ngx_http_complex_value_t));
+        if (ccv.complex_value == NULL) {
+            return NGX_CONF_ERROR;
+        }
+
+        if (ngx_http_compile_complex_value(&ccv) != NGX_OK) {
+            return NGX_CONF_ERROR;
+        }
+
+        pv->filter = ccv.complex_value;
+
+    } else {
+        pv->negative = 0;
+        pv->filter = NULL;
+    }
+
+    return NGX_CONF_OK;
+}
+
+
+static char *
+ngx_http_grpc_var_set(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_proxy_var_set_loc_conf_t *plcf = conf;
+
+    ngx_str_t                           *value;
+    ngx_http_variable_t                 *v;
+    ngx_http_proxy_var_set_variable_t   *pv;
+    ngx_str_t                            s;
+    ngx_http_compile_complex_value_t     ccv;
+
+    value = cf->args->elts;
+
+    if (value[1].data[0] != '$') {
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "invalid variable name \"%V\"", &value[1]);
+        return NGX_CONF_ERROR;
+    }
+
+    value[1].len--;
+    value[1].data++;
+
+    if (plcf->grpc_vars == NGX_CONF_UNSET_PTR) {
+        plcf->grpc_vars = ngx_array_create(cf->pool, 1,
+                                    sizeof(ngx_http_proxy_var_set_variable_t));
+        if (plcf->grpc_vars == NULL) {
+            return NGX_CONF_ERROR;
+        }
+    }
+
+    pv = ngx_array_push(plcf->grpc_vars);
+    if (pv == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    v = ngx_http_add_variable(cf, &value[1], NGX_HTTP_VAR_CHANGEABLE);
+    if (v == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    pv->index = ngx_http_get_variable_index(cf, &value[1]);
+    if (pv->index == NGX_ERROR) {
+        return NGX_CONF_ERROR;
+    }
+
+    if (v->get_handler == NULL) {
+        v->get_handler = ngx_http_proxy_var_set_variable;
+        v->data = (uintptr_t) pv;
+    }
+
+    pv->set_handler = v->set_handler;
+
+    ngx_memzero(&ccv, sizeof(ngx_http_compile_complex_value_t));
+
+    ccv.cf = cf;
+    ccv.value = &value[2];
+    ccv.complex_value = &pv->value;
+
+    if (ngx_http_compile_complex_value(&ccv) != NGX_OK) {
+        return NGX_CONF_ERROR;
+    }
+
+    if (cf->args->nelts == 4) {
+
+        if (ngx_strncmp(value[3].data, "if=", 3) == 0) {
+            s.len = value[3].len - 3;
+            s.data = value[3].data + 3;
+            pv->negative = 0;
+
+        } else if (ngx_strncmp(value[3].data, "if!=", 4) == 0) {
+            s.len = value[3].len - 4;
+            s.data = value[3].data + 4;
+            pv->negative = 1;
+
+        } else {
+            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                "invalid parameter \"%V\"", &value[3]);
             return NGX_CONF_ERROR;
         }
 
@@ -260,7 +451,7 @@ ngx_http_proxy_var_set_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data)
 {
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "log var set variable");
+                   "proxy / grpc var set variable");
 
     v->not_found = 1;
 
@@ -296,35 +487,63 @@ ngx_http_proxy_var_set_merge_loc_conf(ngx_conf_t *cf,
 
     if (conf->vars == NGX_CONF_UNSET_PTR) {
         conf->vars = (prev->vars == NGX_CONF_UNSET_PTR) ? NULL : prev->vars;
-        return NGX_CONF_OK;
-    }
 
-    if (prev->vars == NGX_CONF_UNSET_PTR || prev->vars == NULL) {
-        return NGX_CONF_OK;
-    }
+    } else if (prev->vars != NGX_CONF_UNSET_PTR && prev->vars != NULL) {
 
-    pvars = prev->vars->elts;
-    cvars = conf->vars->elts;
+        pvars = prev->vars->elts;
+        cvars = conf->vars->elts;
 
-    for (i = 0; i < prev->vars->nelts; i++) {
+        for (i = 0; i < prev->vars->nelts; i++) {
 
-        found = 0;
+            found = 0;
 
-        for (j = 0; j < conf->vars->nelts; j++) {
-            if (cvars[j].index == pvars[i].index) {
-                found = 1;
-                break;
+            for (j = 0; j < conf->vars->nelts; j++) {
+                if (cvars[j].index == pvars[i].index) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            if (!found) {
+
+                new_var = ngx_array_push(conf->vars);
+                if (new_var == NULL) {
+                    return NGX_CONF_ERROR;
+                }
+
+                *new_var = pvars[i];
             }
         }
+    }
 
-        if (!found) {
+    if (conf->grpc_vars == NGX_CONF_UNSET_PTR) {
+        conf->grpc_vars = (prev->vars == NGX_CONF_UNSET_PTR) ? NULL : prev->vars;
 
-            new_var = ngx_array_push(conf->vars);
-            if (new_var == NULL) {
-                return NGX_CONF_ERROR;
+    } else if (prev->vars != NGX_CONF_UNSET_PTR && prev->vars != NULL) {
+
+        pvars = prev->vars->elts;
+        cvars = conf->grpc_vars->elts;
+
+        for (i = 0; i < prev->vars->nelts; i++) {
+
+            found = 0;
+
+            for (j = 0; j < conf->grpc_vars->nelts; j++) {
+                if (cvars[j].index == pvars[i].index) {
+                    found = 1;
+                    break;
+                }
             }
 
-            *new_var = pvars[i];
+            if (!found) {
+
+                new_var = ngx_array_push(conf->grpc_vars);
+                if (new_var == NULL) {
+                    return NGX_CONF_ERROR;
+                }
+
+                *new_var = pvars[i];
+            }
         }
     }
 
--- ngx_http_proxy_var_set_module.h
+++ ngx_http_proxy_var_set_module.h
@@ -12,5 +12,6 @@
 #include <ngx_http.h>
 
 ngx_int_t ngx_http_proxy_var_set_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_grpc_var_set_handler(ngx_http_request_t *r);
 
 #endif /* _NGX_HTTP_PROXY_VAR_SET_MODULE_H_INCLUDED_ */
