1cb636ded17e29c34f57376b2b8fe81d47eddeb2
Hanada <im@hanada.info>
2025-01-20 02:06:39 +0800

    fix: add ngx_http_grpc_var_set

--- ngx_http_proxy_var_set_module.c
+++ ngx_http_proxy_var_set_module.c
@@ -22,7 +22,9 @@ typedef struct {
 } ngx_http_proxy_var_set_loc_conf_t;
 
 
-static char * ngx_http_proxy_var_set(ngx_conf_t *cf, ngx_command_t *cmd,
+static char *ngx_http_proxy_var_set(ngx_conf_t *cf, ngx_command_t *cmd,
+    void *conf);
+static char *ngx_http_grpc_var_set(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
 static ngx_int_t ngx_http_proxy_var_set_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
